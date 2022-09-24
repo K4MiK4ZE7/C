186c38a8f9c5fc1f814653bffa46a3d88d7ec5e6
Wragnardev <seker@halithamza.software>
2022-09-24 15:53:45 +0300

    push

--- main0.c
+++ main0.c
@@ -5,8 +5,13 @@ int main()
 {
     char str[100];
     int i;
+    int t;
     printf("Enter a value");
-    scanf("%d", &i);
-    printf("\nYou entered: %d\n", i);
+    scanf("%d", &i, str);
+    printf("\nYou entered: %d:::::%s\n", i, str);
+
+    printf("erhetht4");
+    scanf("%d", &t);
+    printf("\nYou entered: %d:::::%s\n", t);
     return 0;
 }
